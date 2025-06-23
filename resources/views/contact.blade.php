496e64e2824827d3634c5322dec4304f0f55f370
georgemisaell <georgemisaelgantume@gmail.com>
2025-06-23 14:21:22 +0700

    add blade inharitance for navigation

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Contact</title>
-</head>
-<body>
-  <h1>Contact Page</h1>
-</body>
-</html>
+<x-layout :title="$title">
+
+ <p>This is Contact Page</p>
+
+</x-layout>
