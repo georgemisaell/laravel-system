5f54a7a9d792262f197033632f635dbe278f7edb
georgemisaell <georgemisaelgantume@gmail.com>
2025-06-25 00:05:42 +0700

    add model

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    $posts = Post::all();
+    return view('posts', ['title' => 'Blog Page', 'posts' => $posts]);
 });
+Route::get('/posts/{id}', function () {
+    return view('posts', ['title' => 'Blog Page']);
+})->name('post.id');
 
 Route::get('/about', function () {
     return view('about', ['title' => 'About Page']);
