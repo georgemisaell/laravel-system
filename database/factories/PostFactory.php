28dcd602a26b8f3918b915318a5700264faee452
georgemisaell <georgemisaelgantume@gmail.com>
2025-06-26 00:08:52 +0700

    foreign key constraint

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,9 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -22,7 +23,7 @@ class PostFactory extends Factory
         $title = fake()->sentence(mt_rand(3, 8)); // Judul yang lebih natural
         return [
             'title' => $title,
-            'author' => fake()->name(),
+            'author_id' => User::factory(),
             'slug' => Str::slug($title),
             'body' => fake()->paragraph(mt_rand(5, 20)),
         ];
